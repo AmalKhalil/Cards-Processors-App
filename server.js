5c6e75469e75f57615bfdbfc4f922604d48cc9d1
Amal Khalil <Amal.Khalil@itworx.com>
2018-06-04 21:38:29 +0200

    Fix Node Server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '/dist')));
 app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'dist/index.html'));
+   res.sendFile(path.join(__dirname, 'dist/cards-processors-app/index.html'));
  });
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
