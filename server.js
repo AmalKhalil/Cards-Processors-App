80f8b2bc06f64ccbf1ebff97a077ae9930f005e5
Amal Khalil <Amal.Khalil@itworx.com>
2018-06-04 21:27:46 +0200

    Change port As param

--- server.js
+++ server.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 
 var port = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, '/dist')));
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'dist/index.html'));
+ });
 
-app.listen(port, () => console.log('Example app listening on port' + port))
+app.listen(port, () => console.log('Example app listening on port ' + port))
