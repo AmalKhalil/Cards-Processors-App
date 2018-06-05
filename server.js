d91602d6d99500d8b35974fd508288523f7d11e9
Amal Khalil <Amal.Khalil@itworx.com>
2018-06-05 08:11:40 +0200

    Add log message for debuging

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '/dist')));
 app.get('/*', (req, res) => {
-	console.log('__dirname  : '+__dirname);
-   res.sendFile(path.join(__dirname, 'dist/cards-processors-app/index.html'));
+	console.log('req  : '+req.url);
+  // res.sendFile(path.join(__dirname, 'dist/cards-processors-app/index.html'));
  });
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
