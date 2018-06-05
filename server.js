400f2903a5f34c19d40069a5dff9961b97594333
Amal Khalil <Amal.Khalil@itworx.com>
2018-06-05 08:17:44 +0200

    Fix Server.js

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express()
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '/dist')));
 app.get('/*', (req, res) => {
-	console.log('req  : '+req.url);
-  // res.sendFile(path.join(__dirname, 'dist/cards-processors-app/index.html'));
+	console.log('request URL  : '+req.url);
+    
+	if('/' === req.url)
+		res.sendFile(path.join(__dirname, 'dist/cards-processors-app/index.html'));
  });
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
